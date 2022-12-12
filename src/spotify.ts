879725f72558435406a1f0f2e008d3a2097bc88a
huntinator7 <huntinator7@gmail.com>
2022-12-11 20:20:44 -0700

    better logging for whats going on

--- src/spotify.ts
+++ src/spotify.ts
@@ -10,42 +10,55 @@ const spotifyApi = new SpotifyWebApi(spotifyConfig);
 export const getRecentListens = async () => {
   console.log("getRecentListens", new Date().toDateString());
   const users = await db.collection("User").get();
-  console.log("getRecentListens users", users.docs.length);
+  console.log(`gRL users, ${users.docs.length}`);
   getUserRecentListens(users.docs, 0);
 };
 
 const getUserRecentListens = async (users: firestore.QueryDocumentSnapshot<firestore.DocumentData>[], i: number) => {
-  console.log("getUserRecentListens", new Date().toISOString());
+  console.log(`getUserRecentListens, ${i}, ${users.length}`);
   if (i >= users.length) return;
 
   const user = {...users[i].data(), id: users[i].id} as User;
-  console.log(JSON.stringify(user));
+  console.log(`gURL USER, ${JSON.stringify(user)}`);
 
   if (!user.refresh_token) {
+    console.log(`gURL NO TOKEN, ${user.id}`);
     getUserRecentListens(users, i + 1);
   } else {
-    spotifyApi.setRefreshToken(user.refresh_token);
-    const accessTokenRes = await spotifyApi.refreshAccessToken();
-    console.log(accessTokenRes);
-    spotifyApi.setAccessToken(accessTokenRes.body.access_token);
+    try {
+      spotifyApi.setRefreshToken(user.refresh_token);
+      const accessTokenRes = await spotifyApi.refreshAccessToken();
+      console.log(`gURL ACCESS TOKEN RES, ${JSON.stringify(accessTokenRes.body)}`);
+      spotifyApi.setAccessToken(accessTokenRes.body.access_token);
 
-    await getAllRecentlyPlayedByUser(user);
-    setTimeout(() => getUserRecentListens(users, i + 1), 5000);
+      const gARPBU = await getAllRecentlyPlayedByUser(user);
+      console.log(`gARPBU result: ${gARPBU} for ${user.id}`);
+    } catch (e) {
+      console.log(`gURL ERROR, ${JSON.stringify(e)}`);
+    } finally {
+      setTimeout(() => getUserRecentListens(users, i + 1), 5000);
+    }
   }
 };
 
 
-const getAllRecentlyPlayedByUser = async (user: User) => {
-  console.log("getAllRecentlyPlayedByUser");
-  const mostRecent = await spotifyApi.getMyRecentlyPlayedTracks({limit: 50, after: parseInt(user.last_cursor ?? "0")});
-  console.log("mostRecent", mostRecent?.body?.items?.length);
-  const newRecentTracks = mostRecent?.body?.items?.map((t) => cleanTrack(t));
-  newRecentTracks.sort((t1, t2)=> t1.played_at > t2.played_at ? 1 : -1)
-      .forEach((track) => sendTrackToDB(track, user.id));
-  db.collection("User").doc(user.id).update({
-    last_updated: new Date().toISOString(),
-    ...(newRecentTracks.length ? {last_cursor: mostRecent.body.cursors.after} : {}),
-  });
+const getAllRecentlyPlayedByUser = async (user: User): Promise<string> => {
+  console.log(`getAllRecentlyPlayedByUser, ${user.id}`);
+  try {
+    const mostRecent = await spotifyApi.getMyRecentlyPlayedTracks({limit: 50, after: parseInt(user.last_cursor ?? "0")});
+    console.log(`gARPBU mostRecent, ${mostRecent?.body?.items?.length}`);
+    const newRecentTracks = mostRecent?.body?.items?.map((t) => cleanTrack(t));
+    await Promise.all(newRecentTracks.sort((t1, t2)=> t1.played_at > t2.played_at ? 1 : -1)
+        .map((track) => sendTrackToDB(track, user.id)));
+    await db.collection("User").doc(user.id).update({
+      last_updated: new Date().toISOString(),
+      ...(newRecentTracks.length ? {last_cursor: mostRecent.body.cursors.after} : {}),
+    });
+    return "SUCCESS";
+  } catch (e) {
+    console.log(`gARPBU ERROR, ${JSON.stringify(e)}`);
+    return "FAIL";
+  }
 };
 
 const cleanTrack = (track: SpotifyApi.PlayHistoryObject): SpotifyApi.PlayHistoryObject => {
